0f7976b304d2c3ef6a5008bc2c2c190a53e93558
thodorisnikolaidis <thodorisnik@gmail.com>
2025-03-20 14:21:31 +0200

    Minor fix

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,6 +24,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->brandName(str_replace('_', ' ', env('APP_NAME', 'My App')))
             ->id('admin')
             ->path('admin')
             ->login()
