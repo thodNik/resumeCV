0cd56a0719460e52ae42525607df043f9fd32e8d
thodorisnikolaidis <thodorisnik@gmail.com>
2025-03-24 15:07:46 +0200

    Fly pre deploy commit

--- resources/views/components/download-resume-button.blade.php
+++ resources/views/components/download-resume-button.blade.php
@@ -1,7 +1,7 @@
 <button
     type="button"
     wire:click="download"
-    class="py-2 px-6 border-2 border-decoration text-decoration text-sm font-roboto uppercase hover:text-page-bg hover:bg-decoration transition duration-300 ease-in-out"
+    class="cursor-pointer py-2 px-6 border-2 border-decoration text-decoration text-sm font-roboto uppercase hover:text-page-bg hover:bg-decoration transition duration-300 ease-in-out"
 >
     Download Resume
 </button>
